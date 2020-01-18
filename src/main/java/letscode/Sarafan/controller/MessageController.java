e161f589771a8a6dc88299b73a5c43e3c8741dcd
Anton Chesnokov 'Darksiend <logosmk2@gmail.com>
2020-01-18 15:14:17 +0200

    First Commit

---
+++ src/main/java/letscode/Sarafan/controller/MessageController.java
@@ -0,0 +1,29 @@
+package letscode.Sarafan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
+
+public class MessageController {
+
+    public List<Map<String,String>> messages = new ArrayList<Map<String,String>>() {{
+        add(new HashMap<String, String>(){{put("id","1"); put("text","First Message");}});
+        add(new HashMap<String, String>(){{put("id","2"); put("text","Second Message");}});
+        add(new HashMap<String, String>(){{put("id","3"); put("text","Third Message");}});
+
+
+
+    }};
+    @GetMapping
+    public List<Map<String, String>> list(){
+        return messages;
+    }
+}
